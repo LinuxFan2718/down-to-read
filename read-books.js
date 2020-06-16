28c80bf84b9bfc3f88bb986d92d2e09b10842e6a
Dennis Cahillane <dennis@denniscahillane.info>
2020-06-15 21:17:27 -0400

    wip working refactor

--- read-books.js
+++ read-books.js
@@ -14,12 +14,14 @@ chrome.runtime.onMessage.addListener(
   function readBooks() {
     // alert('The DOM is loaded');
     var db = openDatabase('K4W', '3', 'down_to_read', 2 * 1024 * 1024);
+    document.open();
     db.transaction(function (tx) {
         tx.executeSql('SELECT * FROM bookdata;', [], function(tx, results) { 
             var bookTitle = results.rows.item(1)['title'];
-            document.open();
+
             document.write("<h1>" + bookTitle + "</h1>");
-            document.close();
+
         })
     });
+    document.close();
   };
