13e44f879419d463582d9048587f135df9728458
Dennis Cahillane <dennis@denniscahillane.info>
2020-06-21 00:33:48 -0400

    basic rough handling of current url

--- read-books.js
+++ read-books.js
@@ -3,11 +3,13 @@ chrome.runtime.onMessage.addListener(
       if( request.message === "clicked_browser_action" ) {
         console.log(request.url);
         // if on read.amazon.com
-        readBooks()
-        // else
-        // please go to read.amazon.com and try again
-        //chrome.runtime.sendMessage({"message": "open_new_tab", "url": "https://read.amazon.com"});
-        //wait for it to load
+        if(request.url == "https://read.amazon.com/") {
+          readBooks();
+        } else {
+          alert("please go to read.amazon.com and try again");
+          //chrome.runtime.sendMessage({"message": "open_new_tab", "url": "https://read.amazon.com"});
+          //wait for it to load
+        }
       }
     }
   );
